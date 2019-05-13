5001dad9fbaaeccdb9ad983fd03299c5f95153b9
Rene Cocom <fasnimo@gmail.com>
2019-05-13 20:15:14 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,13 +9,22 @@
 def select_winner(passengers)
 winner = ""
 passengers.each do |suite, name|
+<<<<<<< HEAD
   if suite == :suite_a && name.start_with?("A")
+=======
+  if suite == :suite_a &&
+    name.start_with? ("A")
+>>>>>>> 9de728504ef49b8d85fb45d77f6ef7d9d96b0602
     winner = name
   end 
 end
 
+<<<<<<< HEAD
 winner
 end
 
 
 
+=======
+winner
+>>>>>>> 9de728504ef49b8d85fb45d77f6ef7d9d96b0602
